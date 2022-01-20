a5b92eaf15350eae76dc344c48c8376e08e9f7a6
Prisco Coppola <prisco.coppola@soprasteria.com>
2022-01-20 16:26:38 +0100

    fix header and finish DESK

--- Pagina_editoriale/main.js
+++ Pagina_editoriale/main.js
@@ -1,1 +1,19 @@
-console.log('ciao');
+(function($) {
+	
+    $(window).scroll(function() {    
+      var scroll = $(window).scrollTop();
+      var bannerHeight = $(".js--bannerScroll").outerHeight();
+
+  
+      if (scroll > bannerHeight) {
+          $(".header").addClass("new-header");
+          $(".btn-text").addClass("new-btn-text");
+          $(".logo img").addClass("new-img");
+      } else {
+          $(".header").removeClass("new-header");
+          $(".btn-text").removeClass("new-btn-text");
+          $(".logo img").removeClass("new-img");
+      }
+  });
+      
+  })( jQuery );
