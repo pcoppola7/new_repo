bb292899e94cd1143026c24342032d46335e33d2
Prisco Coppola <prisco.coppola@soprasteria.com>
2022-01-21 11:00:59 +0100

    fix

--- Pagina_editoriale/main.js
+++ Pagina_editoriale/main.js
@@ -9,10 +9,14 @@
           $(".header").addClass("new-header");
           $(".btn-text").addClass("new-btn-text");
           $(".logo img").addClass("new-img");
+          $(".header-mobile").addClass("new-header");
+          $(".header-top > i").addClass("new-icon-mobile-u");
       } else {
           $(".header").removeClass("new-header");
           $(".btn-text").removeClass("new-btn-text");
           $(".logo img").removeClass("new-img");
+          $(".header-mobile").removeClass("new-header");
+          $(".header-top > i").removeClass("new-icon-mobile-u");
       }
   });
       
