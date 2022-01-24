62291d139252c8568ac934ddd8543ee5c1234c37
Prisco Coppola <prisco.coppola@soprasteria.com>
2022-01-24 16:07:26 +0100

    js

--- learn_html/main.js
+++ learn_html/main.js
@@ -1,11 +1,17 @@
 
-const clickLink = document.querySelector('.article-link');
+let active = false;
 
-clickLink.addEventListener('click', (e) =>{
-    console.log(e);
-    const active = true;
 
-    if (active) {
-        
-    }
+document.querySelectorAll('.article-link').addEventListener('click', (e) => {
+
+    console.log(e.target);
+    
+    // console.log(document.querySelector('.article-link'));
+    // if (active === true) {
+    //     active = false;
+    //     document.querySelector('.article-link').classList.remove('green')
+    // } else {
+    //     active = true;
+    //     document.querySelector('.article-link').classList.add('green');
+    // }
 })
